e935fc489399d20cbe1dc95d9d78b733fa8bbfc4
serialworm <eric.beringer@thinksolid.com>
2015-01-07 21:24:22 -0600

    Progress commit

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,10 +3,27 @@
 var eventsControllers = angular.module('eventsControllers', []);
 
 eventsControllers.controller('EventsListCtrl', ['$scope', function ($scope) {
-  VividSeats.eventService.all(function(res){
-    $scope.events = res;
-    $scope.$apply();
-  }, function(res) {
-    console.log(res);
-  });
+    VividSeats.eventService.all(function(res){
+      $scope.events = res;
+      $scope.$apply();
+      $scope.class = "active";
+    }, function(res) {
+      console.log(res);
+    });
+
+    $scope.clearFilter = function() {
+      $scope.filters = {};
+    }
+
+    $scope.getUpcomingEvents = function() {
+      $scope.clearFilter();
+      $scope.filters.date > new Date();
+    }
+
+    $scope.getLocalEvents = function() {
+      $scope.clearFilter();
+      $scope.filters.venue = {};
+      $scope.filters.venue.state = 'IL';
+    }
+
 }]);
